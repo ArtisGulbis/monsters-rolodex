0619a4bd603d0bb0da81c46fdbd7a964d67e4489
Artis Gulbis <artux28@hotmail.com>
2020-01-08 21:28:45 +0100

    Updates

---
+++ precache-manifest.90431a638369e87a3acf07403670e236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7deba9f0fa5ca787fbdba408fa8d4c23",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e4c3d884a773d1a87152",
+    "url": "/monsters-rolodex/static/css/main.994733d0.chunk.css"
+  },
+  {
+    "revision": "1093a6240466003a5984",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4c3d884a773d1a87152",
+    "url": "/monsters-rolodex/static/js/main.12bb2f7d.chunk.js"
+  },
+  {
+    "revision": "fcc6c7087f08bfeac99a",
+    "url": "/monsters-rolodex/static/js/runtime-main.52149680.js"
+  }
+]);
